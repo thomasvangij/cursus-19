16de87d8512af31f403d829692052bd919e4b742
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2020-12-06 17:49:16 +0100

    ft_memcpy

--- libft.a/ft_memcpy.c
+++ libft.a/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: tvan-gij </var/mail/tvan-gij>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 15:21:04 by tvan-gij          #+#    #+#             */
-/*   Updated: 2020/12/06 16:04:29 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2020/12/06 17:48:44 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	char 		*dest;
+	char		*dest;
 	const char	*source;
 
 	dest = dst;
