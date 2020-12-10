c17b644f0e125041c9325485c2e42cd0e0b793e4
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2020-12-10 15:18:15 +0100

    ft_memmove

---
+++ libft.a/ft_memmove.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvan-gij </var/mail/tvan-gij>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/10 14:51:42 by tvan-gij          #+#    #+#             */
+/*   Updated: 2020/12/10 15:13:02 by tvan-gij         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*dest;
+	const char	*source;
+
+	dest = dst;
+	source = src;
+	while (len-- > 0)
+		dest[len] = source[len];
+	return (dst);
+}
