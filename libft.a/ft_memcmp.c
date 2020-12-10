1cc77144082973cdfabeb724b7c8c5c9e33189d2
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2020-12-10 16:17:32 +0100

    ft_memcmp

---
+++ libft.a/ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvan-gij </var/mail/tvan-gij>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/10 16:00:09 by tvan-gij          #+#    #+#             */
+/*   Updated: 2020/12/10 16:17:08 by tvan-gij         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (*str1 == *str2 && n != 0)
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+	return (*str1 - *str2);
+}
