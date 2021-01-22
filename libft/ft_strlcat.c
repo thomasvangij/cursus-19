e23d94ea77fd2b76c92a8104e59b449fba56662f
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-01-22 17:45:22 +0100

    libft

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:22:01 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/01/22 16:16:49 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/01/22 17:44:41 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	j = ft_strlenc(src);
 	lendst = ft_strlenc(dst);
-	while (dstsize - lendst - 1 > i && i < j)
-	{
-		dst[lendst + i] = src[i];
-		i++;
-	}
+	if (dstsize > 0 && dstsize - lendst - 1 > 0)
+		while (dstsize - lendst - 1 > i && i < j)
+		{
+			dst[lendst + i] = src[i];
+			i++;
+		}
 	dst[lendst + i] = '\0';
 	return (lendst + j);
 }
