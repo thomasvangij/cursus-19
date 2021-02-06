e41fc7acf30fcf23799e894bcca02282516778a3
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-02-06 15:20:53 +0100

    last fix

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:15:44 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/01/22 16:04:25 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/02/06 15:17:27 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	dest = dst;
 	source = src;
 	while (n-- > 0)
+	{
+		if (!dest && !source)
+			break;
 		dest[n] = source[n];
+	}
 	return (dst);
 }
