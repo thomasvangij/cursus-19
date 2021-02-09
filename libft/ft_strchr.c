72a4313988d46edb682b377a0c42b270a7873067
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-02-09 16:45:27 +0100

    last fix

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,21 +6,21 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:20:46 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/01/17 13:21:12 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/02/09 16:43:41 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
 	int		i;
-	char	occurs;
 	char	*strs;
 
-	i = 0;
-	occurs = (char)c;
+	i = -1;
 	strs = (char *)s;
-	while (strs[i] != occurs)
-		if (strs[++i] == occurs)
+	while (strs[++i] || !c)
+	{
+		if (strs[i] == (char)c)
 			return (strs + i);
+	}
 	return (0);
 }
