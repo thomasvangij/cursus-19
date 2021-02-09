e84a89edaa09898287b8e84dc8e523127929806f
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-02-09 18:04:21 +0100

    last fix

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:23:31 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/01/22 16:22:47 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/02/09 18:03:45 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char	*str1;
-	char	*str2;
 	size_t	i;
 
-	str1 = (char *)s1;
-	str2 = (char *)s2;
 	i = 0;
-	while (str1[i] == str2[i] && i < n && *str1 != '\0')
+	while ((unsigned char)(s1[i]) == (unsigned char)(s2[i]) && i < n && s1[i])
 		i++;
 	if (i == n)
 		i--;
-	return (str1[i] - str2[i]);
+	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
