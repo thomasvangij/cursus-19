3fcee6622554e14196c757d4c6e12f1cdf77cca3
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-03-05 13:39:40 +0100

    libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,11 +6,11 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:13:52 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/01/22 17:16:37 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/03/05 13:39:22 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_itoa(int n)
 {
