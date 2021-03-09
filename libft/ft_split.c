f524ba0558efeb127f02917c8310c7586f47b409
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-03-09 18:09:04 +0100

    libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:20:23 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/03/05 13:28:49 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/03/09 18:06:24 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ char	*ft_strdupp(const char *s, char c, int j)
 	int		w_len;
 
 	w_len = 0;
-	while (!(is_sep(s[j + w_len], c)))
+	while (!(is_sep(s[j + w_len], c)) && s[j + w_len])
 		w_len++;
 	i = -1;
 	if (!(w_str = (char *)malloc(w_len + 1)))
@@ -87,7 +87,7 @@ char	**ft_split(const char *s, char c)
 			split_error(res_tab, i);
 			return (NULL);
 		}
-		while (!(is_sep(s[j], c)))
+		while (!(is_sep(s[j], c)) && s[j])
 			j++;
 	}
 	res_tab[size] = 0;
