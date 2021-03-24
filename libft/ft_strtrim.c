83c4bf0544f28ad6788e6d8363000b0d029367f8
Thomas Van gijsegem <tvan-gij@shi-r1-p7.s19.be>
2021-03-24 16:22:31 +0100

    libft

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,82 +6,57 @@
 /*   By: tvan-gij <tvan-gij@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:24:57 by tvan-gij          #+#    #+#             */
-/*   Updated: 2021/03/05 13:32:11 by tvan-gij         ###   ########.fr       */
+/*   Updated: 2021/03/24 16:18:31 by tvan-gij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_first_count(const char *s1, const char *set)
+size_t			ft_stlen(const char *s)
 {
-	int		j;
-	int		k;
-
-	j = 0;
-	k = -1;
-	while (++k >= 0)
-	{
-		if (s1[j] == set[k])
-		{
-			j++;
-			k = -1;
-		}
-		if (k > 0 && !(set[k]))
-			break ;
-	}
-	return (j);
-}
-
-int		ft_last_count(const char *s1, const char *set)
-{
-	int	i;
-	int	j;
-	int k;
+	size_t	i;
 
 	i = 0;
-	j = 0;
-	k = -1;
-	while (s1[i])
+	while (s[i] != '\0')
 		i++;
-	while (++k >= 0)
-	{
-		if (s1[i - 1] == set[k])
-		{
-			j++;
-			i--;
-			k = -1;
-		}
-		if (k > 0 && !(set[k]))
-			break ;
-	}
-	return (j);
+	return (i);
 }
 
-char	*ft_strtrim(const char *s1, const char *set)
+unsigned int	char_in_set(char c, const char *set)
 {
-	int		count;
-	int		i;
-	int		k;
-	char	*str_t;
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char			*ft_strtrim(const char *s1, const char *set)
+{
+	int				i;
+	int				first;
+	int				last;
+	unsigned int	size;
+	char			*str_t;
 
 	if (!s1 || !set)
 		return (NULL);
-	k = ft_last_count(s1, set);
-	i = ft_first_count(s1, set);
-	count = 0;
-	while (s1[count])
-		count++;
-	count = count - k - i;
-	if (count < 0)
-		count = 0;
-	if (!(str_t = (char *)malloc(count + 1)))
+	i = 0;
+	while (s1[i] && char_in_set(s1[i], set))
+		i++;
+	first = i;
+	if ((i = ft_stlen(s1) - 1) != -1)
+		while (i >= 0 && char_in_set(s1[i], set))
+			i--;
+	last = i;
+	size = last - first + 2;
+	if (!(str_t = (char *)malloc(sizeof(char) * size)))
 		return (NULL);
-	k = 0;
-	while (s1[i + k] && k < count)
-	{
-		str_t[k] = (char)(s1[i + k]);
-		k++;
-	}
-	str_t[k] = '\0';
+	i = first - 1;
+	while (++i <= last)
+		str_t[i - first] = s1[i];
+	str_t[i - first] = '\0';
 	return (str_t);
 }
